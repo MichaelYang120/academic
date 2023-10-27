3fde1565224f6849c8a0dc6d714967853ca88fb7
MichaelYang120 <michaelyangiscoding@gmail.com>
2023-10-26 22:02:10 -0500

    fixed functionality to read current balance

--- sophia/introToJavaTouchstone/Main.java
+++ sophia/introToJavaTouchstone/Main.java
@@ -8,11 +8,11 @@ public class Main {
 		Scanner scanner = new Scanner(System.in);
 		int budget = 0;
 
-		displayMessage("Please enter your starting balance");
+		System.out.println("Please enter your starting balance");
 		try {
 			budget= scanner.nextInt();
 			if(budget == 0) {
-				displayMessage("Please enter a vaild starting balance");
+				System.out.println("Please enter a vaild starting balance");
 			}
 		} catch (Exception e) {
 		    System.out.println("Please enter a number without a decimal");
@@ -23,21 +23,21 @@ public class Main {
 			return;
 		}
 
-		System.out.println("your current balance is " + budget);
-		
-		displayMessage("Enter spending amount");
+		do {
 
-		int spendingAmount = scanner.nextInt();
-		if(spendingAmount > budget) {
-			displayMessage("You do not have enough money to spend that amount");
-			return;
-		}
+			System.out.println("your current balance is " + budget);
+			System.out.println("Enter spending amount");
 
-		int newBalance = budget - spendingAmount;
-		System.out.println("Your new balance is " + newBalance);
-	}
-	static void displayMessage(String message) {
-		System.out.println(message);
+			int spendingAmount = scanner.nextInt();
+			if(spendingAmount > budget) {
+				System.out.println("You do not have enough money to spend that amount");
+				return;
+			}
+
+			int newBalance = budget - spendingAmount;
+
+			budget = newBalance;	
+		} while (balanceCheck);
 	}
 
 	public static boolean verifyBalance(int balance) {
