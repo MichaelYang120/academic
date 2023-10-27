afe9f1a57424f17cd3ec22f0af284cd148348d99
MichaelYang120 <michaelyangiscoding@gmail.com>
2023-10-26 21:41:46 -0500

    remove comments

--- sophia/introToJavaTouchstone/Main.java
+++ sophia/introToJavaTouchstone/Main.java
@@ -1,13 +1,6 @@
 import java.awt.*;
-import java.util.Arrays;
-import java.util.Date;
 import java.util.Scanner;
 
-
-// for the class 
-
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
 
 	public static void main(String[] args) {
