4b4f405c9924fcbe119a7afe460c47054befd09b
MichaelYang120 <michaelyangiscoding@gmail.com>
2023-10-29 10:53:23 -0500

    fixed if statements for clarity

--- sophia/introToPythonTouchstone/main.py
+++ sophia/introToPythonTouchstone/main.py
@@ -1,7 +1,11 @@
 import random
 def main():
+    #debug turn on to true to see debug messages
+    debug = True
     # create a random number from 0 - 10
     random_number = random.randint(0, 10)
+    if debug:
+        print("check random number: " + str(random_number))
     # create a variable to store the user's guess
     guess_correct = False
     # create a variable to store the number of guesses
@@ -14,15 +18,23 @@ def main():
         number_of_guesses += 1
         # check if the user's guess is too low
         if guess < random_number:
+            # check if guess is out of range
+            if guess < 0:
+                print("Your guess is too low please guess a number greater than 0")
             # tell the user their guess is too low
-            print("Your guess is too low")
+            else:
+                print("Your guess is too low")
         # check if the user's guess is too high
         elif guess > random_number:
+            # check if guess is out of range
+            if guess > 10:
+                print("Your guess is too high please guess a number less than 10")
             # tell the user their guess is too high
-            print("Your guess is too high")
+            else:
+                print("Your guess is too high")
         elif guess == random_number:
             print("You guessed the number!")
+            # tell the user how many guesses it took them
             print("It took you " + str(number_of_guesses) + " guesses")
             guess_correct = True
-
 main()
